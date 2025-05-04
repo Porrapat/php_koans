e43f6a39e1b789525a22e3ae31cdf24bc7329ebd
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2025-05-04 13:33:27 +0700

    Update file!

--- path_to_enlightenment.php
+++ path_to_enlightenment.php
@@ -4,27 +4,51 @@ require 'vendor/autoload.php';
 
 $koans = glob(__DIR__ . '/koans/*.php');
 
-$total = count($koans);
-$current = 0;
+// Count all test methods
+function countTestMethodsInFiles(array $files): int {
+    $total = 0;
+    foreach ($files as $file) {
+        $contents = file_get_contents($file);
+        preg_match_all('/function\s+(test\w+)\s*\(/', $contents, $matches);
+        $total += count($matches[1]);
+    }
+    return $total;
+}
+
+$totalTests = countTestMethodsInFiles($koans);
+$completedTests = 0;
 
 foreach ($koans as $file) {
-    $current++;
-
     echo "Running: $file\n";
 
     $output = [];
     $exitCode = 0;
 
-    exec("vendor\bin\phpunit.bat --colors=never --filter '' $file", $output, $exitCode);
     // exec("vendor/bin/phpunit --colors=never --filter '' $file", $output, $exitCode);
+    exec("vendor\bin\phpunit.bat --colors=never --filter '' $file", $output, $exitCode);
+   
+    // Count how many passed
+    $passed = 0;
+    foreach ($output as $line) {
+        if (preg_match('/OK \((\d+) test/', $line, $matches)) {
+            $passed = (int)$matches[1];
+            break;
+        }
+    }
+
+    $completedTests += $passed;
 
     if ($exitCode !== 0) {
-        echo "About" . basename($file, '.php') . " has damaged your karma.\n";
+        echo "\nAbout" . basename($file, '.php') . " has damaged your karma.\n";
         echo "\nThe Master says:\n  You have not yet reached enlightenment.\n";
+
         echo "\nThe answers you seek...\n";
 
         foreach ($output as $line) {
-            if (stripos($line, 'FAILURES!') !== false || stripos($line, 'FAILURE') !== false || stripos($line, 'Error') !== false) {
+            if (stripos($line, 'FAILURES!') !== false ||
+                stripos($line, 'ERROR') !== false ||
+                stripos($line, 'FAILURE') !== false ||
+                stripos($line, 'AssertionFailedError') !== false) {
                 echo "  $line\n";
             }
         }
@@ -32,10 +56,9 @@ foreach ($koans as $file) {
         echo "\nPlease meditate on the following code:\n";
         echo "  $file\n";
 
-        $bar = str_repeat('X', $current - 1) . '_' . str_repeat('_', $total - $current);
+        $bar = str_repeat('X', $completedTests) . str_repeat('_', $totalTests - $completedTests);
         echo "\nmountains are merely mountains\n";
-        echo "your path thus far [$bar] $current/$total\n";
-
+        echo "your path thus far [$bar] $completedTests/$totalTests\n";
         exit(1);
     }
 }
