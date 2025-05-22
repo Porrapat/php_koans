d73306479c8d2ca26e75744006829d8fafd6bfa4
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2025-05-22 12:55:30 +0700

    Need clean up.

---
+++ koansSolutions/AboutClassMethodTest.php
@@ -0,0 +1,138 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+// class Dog
+// {
+//     public function name()
+//     {
+//         return "Fido";
+//     }
+
+//     private function tail()
+//     {
+//         return "tail";
+//     }
+// }
+
+class AboutClassMethodTest extends TestCase
+{
+    public static function my_global_method($a, $b)
+    {
+        return $a + $b;
+    }
+
+    public function test_calling_global_methods()
+    {
+        $this->assertEquals(5, self::my_global_method(2, 3));
+    }
+
+    public function test_calling_global_methods_without_parentheses()
+    {
+        $result = self::my_global_method(2, 3);
+        $this->assertEquals(5, $result);
+    }
+
+    public function test_sometimes_missing_parentheses_are_ambiguous()
+    {
+        // PHP requires parentheses, so ambiguity like Ruby does not exist.
+        $this->assertEquals(5, self::my_global_method(2, 3));
+    }
+
+    public function test_calling_global_methods_with_wrong_number_of_arguments()
+    {
+        $this->expectException(ArgumentCountError::class);
+        self::my_global_method();
+
+        $this->expectException(ArgumentCountError::class);
+        self::my_global_method(1, 2, 3);
+    }
+
+    public static function method_with_defaults($a, $b = 'default_value')
+    {
+        return [$a, $b];
+    }
+
+    public function test_calling_with_default_values()
+    {
+        $this->assertEquals([1, 'default_value'], self::method_with_defaults(1));
+        $this->assertEquals([1, 2], self::method_with_defaults(1, 2));
+    }
+
+    public static function method_with_var_args(...$args)
+    {
+        return $args;
+    }
+
+    public function test_calling_with_variable_arguments()
+    {
+        $this->assertEquals(__, gettype(self::method_with_var_args()));
+        $this->assertEquals(__, self::method_with_var_args());
+        $this->assertEquals(__, self::method_with_var_args('one'));
+        $this->assertEquals(__, self::method_with_var_args('one', 'two'));
+    }
+
+    public static function method_with_explicit_return()
+    {
+        return 'return_value';
+    }
+
+    public function test_method_with_explicit_return()
+    {
+        $this->assertEquals(__, self::method_with_explicit_return());
+    }
+
+    public static function method_without_explicit_return()
+    {
+        return 'return_value';
+    }
+
+    public function test_method_without_explicit_return()
+    {
+        $this->assertEquals(__, self::method_without_explicit_return());
+    }
+
+    public function my_method_in_the_same_class($a, $b)
+    {
+        return $a * $b;
+    }
+
+    public function test_calling_methods_in_same_class()
+    {
+        $this->assertEquals(__, $this->my_method_in_the_same_class(3, 4));
+    }
+
+    public function test_calling_methods_in_same_class_with_explicit_receiver()
+    {
+        $this->assertEquals(__, $this->my_method_in_the_same_class(3, 4));
+    }
+
+    private function my_private_method()
+    {
+        return "a secret";
+    }
+
+    public function test_calling_private_methods_without_receiver()
+    {
+        $this->assertEquals(__, $this->my_private_method());
+    }
+
+    public function test_calling_private_methods_with_an_explicit_receiver()
+    {
+        $this->assertEquals(__, $this->my_private_method());
+    }
+
+    // public function test_calling_methods_in_other_objects_require_explicit_receiver()
+    // {
+    //     $rover = new self::Dog();
+    //     $this->assertEquals(__, $rover->name());
+    // }
+
+    // public function test_calling_private_methods_in_other_objects()
+    // {
+    //     $rover = new self::Dog();
+    //     $this->expectException(Error::class);
+    //     $rover->tail();
+    // }
+}
+
---
+++ koansSolutions/AboutNamespaceTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Koans;
+
+use PHPUnit\Framework\TestCase;
+
+trait Nameable
+{
+    protected string $name;
+
+    public function setName(string $newName): void
+    {
+        $this->name = $newName;
+    }
+
+    public function here(): string
+    {
+        return 'in_trait';
+    }
+}
+
+class DogUsingNameable
+{
+    use Nameable;
+
+    public function __construct()
+    {
+        $this->name = "Fido";
+    }
+
+    public function bark(): string
+    {
+        return "WOOF";
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function here(): string
+    {
+        return 'in_class';
+    }
+}
+
+class AboutNamespaceTest extends TestCase
+{
+    // public function test_traits_cannot_be_instantiated()
+    // {
+    //     $this->expectError(); // Because PHP will emit a fatal error for instantiating traits
+    //     $trait = new \Koans\Nameable();
+    // }
+
+    public function test_normal_methods_are_available_in_the_object()
+    {
+        $dog = new \Koans\DogUsingNameable();
+        $this->assertEquals("WOOF", $dog->bark());
+    }
+
+    public function test_trait_methods_are_also_available_in_the_object()
+    {
+        $dog = new \Koans\DogUsingNameable();
+        $this->assertNull($dog->setName("Rover"));
+    }
+
+    public function test_trait_methods_can_affect_instance_variables_in_the_object()
+    {
+        $dog = new \Koans\DogUsingNameable();
+        $this->assertEquals("Fido", $dog->getName());
+        $dog->setName("Rover");
+        $this->assertEquals("Rover", $dog->getName());
+    }
+
+    public function test_classes_can_override_trait_methods()
+    {
+        $dog = new \Koans\DogUsingNameable();
+        $this->assertEquals("in_class", $dog->here());
+    }
+}
