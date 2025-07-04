69bd27cd458da974231e214217854e310c5ec04b
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2025-07-04 12:42:54 +0700

    Add AboutIsEvenMiniProject and change order of some line.

---
+++ koans/AboutIsEvenMiniProject.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Koans;
+use PHPUnit\Framework\TestCase;
+
+function is_even(int $number): bool
+{
+    return __;
+}
+
+/**
+ * @testdox Write the function that check it is even number or not
+ */
+class AboutIsEvenMiniProject extends TestCase
+{
+    /**
+     * @testdox Is even number
+     */
+    public function testIsEvenNumber(): void
+    {
+        $this->assertTrue(is_even(0));
+        $this->assertTrue(is_even(2));
+        $this->assertTrue(is_even(200));
+        $this->assertTrue(is_even(5000));
+        $this->assertTrue(is_even(1000000));
+        $this->assertTrue(is_even(-4));
+        $this->assertTrue(is_even(-400));
+        $this->assertTrue(is_even(-1000));
+        $this->assertTrue(is_even(-100000));
+    }
+
+    /**
+     * @testdox Is odd number
+     */
+    public function testIsOddNumber(): void
+    {
+        $this->assertFalse(is_even(1));
+        $this->assertFalse(is_even(3));
+        $this->assertFalse(is_even(101));
+        $this->assertFalse(is_even(301));
+        $this->assertFalse(is_even(100001));
+        $this->assertFalse(is_even(-1));
+        $this->assertFalse(is_even(-100001));
+    }
+}
