fa2a8df754975b2c9d2a45c3024afd73cae68a42
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2025-10-15 15:14:10 +0700

    Remove old not used bootstrap file here!

--- bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Autoload Composer
-require_once __DIR__ . '/vendor/autoload.php';
-
-// Define the koan placeholder
-define('__', '__FILL_ME_IN__');
-define('____', !('__FILL_ME_IN__'));
